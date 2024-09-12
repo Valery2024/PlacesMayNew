27bba5452238dc8e51c546369e8174e1378dd416
Valery <Valery@Egors-MacBook-Pro-2.local>
2024-09-12 11:49:14 +0300

    Определил: Лаунчсккреен и высоту ячейки, изо, и обрезал картинку

--- PlacesMayNew/MainViewController.swift
+++ PlacesMayNew/MainViewController.swift
@@ -9,37 +9,43 @@ import UIKit
 
 class MainViewController: UITableViewController {
     let restauratnNames = ["Балкан Гриль", "Бочка",
-                              "Вкусные истории", "Дастархан",
-                              "Индокитай", "Классик", "Шок",
-                              "Bonsai","Burger Heroes", "Kitchen","Love&Life","Morris Pub","Sherlock Holmes","Speak Easy","X.O"
-       ]
-       
+                           "Вкусные истории", "Дастархан",
+                           "Индокитай", "Классик", "Шок",
+                           "Bonsai","Burger Heroes", "Kitchen","Love&Life","Morris Pub","Sherlock Holmes","Speak Easy","X.O"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
-    // MARK: - Table view data source
-//здесь не нужно расширение так этот класс уже подписан под протоколы/ Eще нужно использованть команду comand + T-она позволяет закрепить нужные файлы сверху на вкладке
     
-
+    // MARK: - Table view data source
+    //здесь не нужно расширение так этот класс уже подписан под протоколы/ Eще нужно использованть команду comand + T-она позволяет закрепить нужные файлы сверху на вкладке
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return restauratnNames.count
     }
-
-  
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Configure the cell...
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-               cell.textLabel?.text = restauratnNames[indexPath.row]
-               cell.imageView?.image = UIImage(named: restauratnNames[indexPath.row])
-       
+        cell.textLabel?.text = restauratnNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restauratnNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.height/2
+        cell.imageView?.clipsToBounds = true//обрезает или показывает наш круг
         
         return cell
         
     }
+    //MARK: TableView delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        85
+    }
+}
   /*cell.textLabel?.text - это свойство отражает в ячейке текстовую информацию в нашем случае наименование ресторана. detailTextLabel-отображает более детальную информацию . imageView?- отображает изображение в нашей ячейке. Все эти штуки прикрепляютчся к contentview*/
 
     /*
@@ -87,4 +93,3 @@ class MainViewController: UITableViewController {
     }
     */
 
-}
