a0e7c1016eb7e09377f4bc531121ba9b9d8dad78
Valery <Valery@Egors-MacBook-Pro-2.local>
2024-09-13 17:51:24 +0300

    Commit: добавлен метод для выбора камеры, фото, , -cansel

--- PlacesMayNew/MainViewController.swift
+++ PlacesMayNew/MainViewController.swift
@@ -99,3 +99,4 @@ class MainViewController: UITableViewController {
     }
     */
 
+
--- PlacesMayNew/NewPlaceViewController.swift
+++ PlacesMayNew/NewPlaceViewController.swift
@@ -19,7 +19,24 @@ class NewPlaceViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //создаем условие
         if indexPath.row == 0 {//если это первая ячейка-тогда мы вызываем меню для того чтобы пользователь выбрал изо а иначе клава должна скрыться
-         
+            let actioncheet = UIAlertController(title: nil,
+                                             message:nil,
+                                             preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                //TODO: здесь нужно будет создать метод для выбора изо-func chooseImagePicker
+                self.chooseImagePicker(sourse: .camera)
+            }
+                let foto = UIAlertAction(title: "Фото", style: .default) { _ in
+                    self.chooseImagePicker(sourse: .photoLibrary)
+                   
+            }
+                    
+            let cansel = UIAlertAction(title: "Cansel", style: .cancel)
+                    
+            actioncheet.addAction(camera)
+            actioncheet.addAction(foto)
+            actioncheet.addAction(cansel)
+        present(actioncheet, animated: true)
             
         }else{
             view.endEditing(true)
@@ -28,10 +45,23 @@ class NewPlaceViewController: UITableViewController {
     }
     
 }
+
+//MARK: Work with image kklj
+
+extension NewPlaceViewController {
+    func chooseImagePicker(sourse: UIImagePickerController.SourceType) {//sourse:-параметр источник для выбора изо
+        if UIImagePickerController.isSourceTypeAvailable(sourse) {
+            let imagePicker = UIImagePickerController()//создали экз класса
+            imagePicker.allowsEditing = true//рудактирование изо пользователем
+            imagePicker.sourceType = sourse
+            present(imagePicker, animated: true )
+        }
+    }
+}
     // MARK: - TextFiald delegate
-    
+   
     extension NewPlaceViewController: UITextFieldDelegate {
-      //скрываем клавиатуру по нажадтию на дано
+      //скрываем клавиатуру по нажатию на дано
         func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             textField.resignFirstResponder()
             
